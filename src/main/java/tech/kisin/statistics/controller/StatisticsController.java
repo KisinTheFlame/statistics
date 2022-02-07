a4409b5b760c3e5a23e9b4cf4a958a5251af2ca3
KisinTheFlame <kisintheflame@gmail.com>
2022-02-07 17:12:34 +0800

    Clean unwanted

--- src/main/java/tech/kisin/statistics/controller/StatisticsController.java
+++ src/main/java/tech/kisin/statistics/controller/StatisticsController.java
@@ -2,7 +2,6 @@ package tech.kisin.statistics.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import tech.kisin.statistics.service.VisitorCountService;
 
