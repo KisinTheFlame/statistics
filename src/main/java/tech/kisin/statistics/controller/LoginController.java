42a05c86e007541a0d72ad24077a60c041f4536b
KisinTheFlame <870853294@qq.com>
2022-02-15 01:05:45 +0800

    Update

--- src/main/java/tech/kisin/statistics/controller/LoginController.java
+++ src/main/java/tech/kisin/statistics/controller/LoginController.java
@@ -6,17 +6,18 @@ import org.springframework.web.bind.annotation.RestController;
 import tech.kisin.statistics.result.Result;
 import tech.kisin.statistics.result.ResultCode;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class LoginController {
     @PostMapping("/register")
-    public Result<Boolean> register(HttpServletResponse response) {
+    public Result<Boolean> register(HttpServletRequest request, HttpServletResponse response) {
         return new Result<>(ResultCode.SUCCESS, true);
     }
 
     @PostMapping("/login")
-    public Result<Boolean> login(HttpServletResponse response) {
+    public Result<Boolean> login(HttpServletRequest request, HttpServletResponse response) {
         return new Result<>(ResultCode.SUCCESS, true);
     }
 }
